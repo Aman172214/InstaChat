78137b603a8892ec6b28e3080abf226f28bd3d9b
Aman <Sharmaamqn@gmail.com>
2023-12-04 15:59:23 +0530

    add vercel config

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://insta-chat-frontend.vercel.app",
+    origin: ["https://insta-chat-frontend.vercel.app"],
     credentials: true,
   })
 );
